1d5bde1f1fff5d34db0c3fed5cc8158443c07617
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-20 22:50:09 +0200

    Agregar cabecera

--- Front/Views/Sections/cabecera.php
+++ Front/Views/Sections/cabecera.php
@@ -1,4 +1,19 @@
 <section class="cabecera">
-    
+    <div class="containerduo">
+        <div class="flexcentercolumn duo">
+            <h1>DESARROLLO WEB PROFESIONAL</h1>
+            <h2>InsanityDev tiene todo lo que necesitas a precios asequibles</h2>
+            <ul>
+                <li><i class="fas fa-check fa-1x"></i>Diseños personalizados</li>
+                <li><i class="fas fa-check fa-1x"></i>Adaptables a todos los dispositivos</li>
+                <li><i class="fas fa-check fa-1x"></i>Posicionamiento en Google</li>
+                <li><i class="fas fa-check fa-1x"></i>eCommerce</li>
+                <li><i class="fas fa-check fa-1x"></i>Cartas QR</li>
+            </ul>
 
+        </div>
+        <div class="flexcentercolumn duo">
+            <img src="Front/Public/Img/pcwoman.png" alt="mujer trabajadora" class="imgduo">
+        </div>
+    </div>
 </section>
