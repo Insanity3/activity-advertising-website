98876196f12a711a35db9df639d4c5c4c7dd8728
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-20 06:21:26 +0200

    mockup footer

---
+++ Front/Views/Components/footer.php
@@ -0,0 +1,23 @@
+<section class="footer">
+    <div class="rrss flexcentercolumn">
+        <p class="textcenter">Visita mis redes sociales</p>
+        <div class="irrss">
+            <a href="http://www.facebook.com/cristina.oreja.56/">
+            <i class="fab fa-facebook-square fa-3x"></i>
+            </a>
+            <a href="http://github.com/Insanity3">
+            <i class="fab fa-github-square fa-3x"></i>
+            </a>
+            <a href="https://www.instagram.com/_insanity_q/?hl=es">
+            <i class="fab fa-instagram-square fa-3x"></i>
+            </a>
+            <a href="http://">
+            <i class="fab fa-twitter-square fa-3x"></i>
+            </a>
+        </div>
+    </div>
+    
+    <div class="creator flexcentercolumn">
+        <p class="textcenter">Desarrollado por Insanity</p>
+    </div>
+</section>
