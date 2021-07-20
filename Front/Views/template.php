a71d6e8569c46cabeaba896db9427ccc4750eabd
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-20 05:10:52 +0200

    Estructura template

---
+++ Front/Views/template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InsanityDev Desarrollo Web Profesional</title>
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="Front/Public/Plugins/fontawesome5/css/all.css">
+    <!-- Estilos -->
+
+    <!-- Fuentes -->
+</head>
+<body>
+    <header>
+        <?php
+        //Incluimos el header
+        ?>
+    </header>
+    <main>
+        <?php
+        //Incluimos todas las secciones de la web
+        ?>
+    </main>
+    <footer>
+        <?php
+        //Incluimos el footer
+        ?>
+    </footer>
+    
+    <!-- Scripts -->
+
+</body>
+</html>
