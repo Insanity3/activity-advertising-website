da6266d656860495e64a09a5799dcba991b67388
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-20 05:12:48 +0200

    Agregar archivo estilos css

--- Front/Views/template.php
+++ Front/Views/template.php
@@ -8,7 +8,7 @@
     <!-- FontAwesome -->
     <link rel="stylesheet" href="Front/Public/Plugins/fontawesome5/css/all.css">
     <!-- Estilos -->
-
+    <link rel="stylesheet" href="Front/Public/Css/estilos.css">
     <!-- Fuentes -->
 </head>
 <body>
