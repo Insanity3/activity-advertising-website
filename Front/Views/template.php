736c6e66991ddd8e89251f88c465d03c518da817
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-21 03:13:03 +0200

    Añadir fuente

--- Front/Views/template.php
+++ Front/Views/template.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="Front/Public/Css/reset.css">
     <link rel="stylesheet" href="Front/Public/Css/estilos.css">
     <!-- Fuentes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
