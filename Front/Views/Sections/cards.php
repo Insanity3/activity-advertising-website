6e842095058a2728e7d15e3dac29993553ea96df
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-21 13:20:28 +0200

    Agregar sección cards

---
+++ Front/Views/Sections/cards.php
@@ -0,0 +1,79 @@
+<section class="flexcentercolumn">
+    <div class="separator">
+        <h3 class="textcenter">¡Pulsa en las cartas para saber más!</h3>
+    </div>
+
+<div class="cards">
+<a href="">
+        <div class="flexcentercolumn card">
+            <div class="flexcentercolumn duovertical">
+                <i class="fas fa-qrcode fa-8x"></i>
+            </div>
+            <div class="flexcentercolumn duovertical">
+                <div class="flexcentercolumn duovertical">
+                    <h3>-Carta QR-</h3>
+                </div>
+                <div class="flexcentercolumn duovertical">
+                    <p class="textcenter">Personalizarmos tu carta online al detalle para tu bar, cafetería o restaurante.</p>
+                </div>
+                
+            </div>
+        </div>
+    </a>
+
+
+    <a href="">
+        <div class="flexcentercolumn card">
+            <div class="flexcentercolumn duovertical">
+            <i class="fas fa-shopping-cart fa-8x"></i>
+            </div>
+            <div class="flexcentercolumn duovertical">
+                <div class="flexcentercolumn duovertical">
+                    <h3>-eCommerce-</h3>
+                </div>
+                <div class="flexcentercolumn duovertical">
+                    <p class="textcenter">Tendrás una tienda online basada en tus necesidades.</p>
+                </div>
+                
+            </div>
+        </div>
+    </a>
+
+
+    <a href="">
+        <div class="flexcentercolumn card">
+            <div class="flexcentercolumn duovertical">
+            <i class="fas fa-magic fa-8x"></i>
+            </div>
+            <div class="flexcentercolumn duovertical">
+                <div class="flexcentercolumn duovertical">
+                    <h3>-Web anticuada-</h3>
+                </div>
+                <div class="flexcentercolumn duovertical">
+                <p class="textcenter">Le daremos un buen lavado de cara a tu web para que se adapte a las nuevas tendencias.</p>
+                </div>
+                
+            </div>
+        </div>
+    </a>
+
+
+    <a href="">
+        <div class="flexcentercolumn card">
+            <div class="flexcentercolumn duovertical">
+            <i class="fas fa-palette fa-8x"></i>
+            </div>
+            <div class="flexcentercolumn duovertical">
+                <div class="flexcentercolumn duovertical">
+                    <h3>-Nueva web-</h3>
+                </div>
+                <div class="flexcentercolumn duovertical">
+                <p class="textcenter">Diseñaremos una web acorde a tus gustos y tus necesidades.</p>
+                </div>
+            </div>
+        </div>
+    </a>
+</div>
+    
+
+</section>
