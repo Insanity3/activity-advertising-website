007d69486df52a37d6966acad4e59ee49377c486
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-22 20:17:25 +0200

    sección carta qr v1

---
+++ Front/Views/Sections/qr.php
@@ -0,0 +1,36 @@
+<section>
+<a name="qr"></a>
+    <div class="container">
+        <div class="containerduo">
+            <div class="flexcentercolumn duo back">
+                <div class="slidercontenedor">
+                    <div class="contenidoslider flexcentercolumn duo">
+                        <img src="Front/Public/Img/mockupmovil.png" alt="smartphone" class="movil">
+                    </div>
+                    <div class="contenidoslider flexcentercolumn duo">
+                        <img src="Front/Public/Img/mockupmovil1.png" alt="smartphone" class="movil">
+                    </div>
+                    <div class="contenidoslider flexcentercolumn duo">
+                        <img src="Front/Public/Img/mockupmovil2.png" alt="smartphone" class="movil">
+                    </div>
+                    <div class="contenidoslider flexcentercolumn duo">
+                        <img src="Front/Public/Img/mockupmovil.png" alt="smartphone" class="movil">
+                    </div>
+                </div>
+            </div>
+            <div class="flexcentercolumn duo color">
+                
+                <h2>Carta QR</h2>
+                <h3 class="textcenter">¿Tu bar o restaurante necesita una carta online?</h3>
+                <h4>Te ofrecemos:</h4>
+                <ul>
+                    <li><i class="fas fa-check fa-1x"></i>Diseño de carta adaptado al estilo de tu local</li>
+                    <li><i class="fas fa-check fa-1x"></i>Si no dispones de un logo ¡Te lo diseñamos!</li>
+                    <li><i class="fas fa-check fa-1x"></i>Podrás administrar la carta desde tu móvil u ordenador</li>
+                    <li><i class="fas fa-check fa-1x"></i>La carta estará en tantos idiomas como necesites</li>
+                    <li><i class="fas fa-check fa-1x"></i>Control de alérgenos: podrás añadirlos a cada plato desde la administración</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
