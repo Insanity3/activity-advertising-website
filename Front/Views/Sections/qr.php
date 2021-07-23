bae75ae8fa677709a27c17e0fcfc37a0a0a4ae36
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-23 12:28:56 +0200

    Actualización qr para movil

--- Front/Views/Sections/qr.php
+++ Front/Views/Sections/qr.php
@@ -3,6 +3,19 @@
     <div class="container">
         <div class="containerduo">
             <div class="flexcentercolumn duo back">
+                    <h2>Carta QR</h2>
+                    <i class="fas fa-qrcode fa-8x imovil"></i>
+                    <h3 class="textcenter">¿Tu bar o restaurante necesita una carta online?</h3>
+                    <h4>Te ofrecemos:</h4>
+                    <ul>
+                        <li><i class="fas fa-check fa-1x"></i>Diseño de carta adaptado al estilo de tu local</li>
+                        <li><i class="fas fa-check fa-1x"></i>Si no dispones de un logo ¡Te lo diseñamos!</li>
+                        <li><i class="fas fa-check fa-1x"></i>Podrás administrar la carta desde tu móvil u ordenador</li>
+                        <li><i class="fas fa-check fa-1x"></i>La carta estará en tantos idiomas como necesites</li>
+                        <li><i class="fas fa-check fa-1x"></i>Control de alérgenos: podrás añadirlos a cada plato desde la administración</li>
+                    </ul>
+            </div>
+            <div class="flexcentercolumn duo color">
                 <div class="slidercontenedor">
                     <div class="contenidoslider flexcentercolumn duo">
                         <img src="Front/Public/Img/mockupmovil.png" alt="smartphone" class="movil">
@@ -17,19 +30,6 @@
                         <img src="Front/Public/Img/mockupmovil.png" alt="smartphone" class="movil">
                     </div>
                 </div>
-            </div>
-            <div class="flexcentercolumn duo color">
-                
-                <h2>Carta QR</h2>
-                <h3 class="textcenter">¿Tu bar o restaurante necesita una carta online?</h3>
-                <h4>Te ofrecemos:</h4>
-                <ul>
-                    <li><i class="fas fa-check fa-1x"></i>Diseño de carta adaptado al estilo de tu local</li>
-                    <li><i class="fas fa-check fa-1x"></i>Si no dispones de un logo ¡Te lo diseñamos!</li>
-                    <li><i class="fas fa-check fa-1x"></i>Podrás administrar la carta desde tu móvil u ordenador</li>
-                    <li><i class="fas fa-check fa-1x"></i>La carta estará en tantos idiomas como necesites</li>
-                    <li><i class="fas fa-check fa-1x"></i>Control de alérgenos: podrás añadirlos a cada plato desde la administración</li>
-                </ul>
             </div>
         </div>
     </div>
