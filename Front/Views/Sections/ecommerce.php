fb45e5e09c6486cc82d628a05cebf33ddb51de04
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-23 14:53:42 +0200

    Añadir ecommerce v1

---
+++ Front/Views/Sections/ecommerce.php
@@ -0,0 +1,28 @@
+<section>
+<a name="ecommerce"></a>
+    <div class="container">
+        <div class="containerduo">
+            <div class="flexcentercolumn duo color dos">
+                <a href="https://www.abstractinsanity.com" class="acenter">
+                    <img src="Front/Public/Img/prestashop.PNG" class="shop" alt="Prestashop">
+                </a>
+                
+            </div>
+            <div class="flexcentercolumn duo back uno">
+                    <h2>Tienda online</h2>
+                    <i class="fas fa-euro-sign fa-8x imovil"></i>
+                    <h3 class="textcenter">¡El eCommerce está en auge!</h3>
+                    <h4>Te ofrecemos:</h4>
+                    <ul>
+                        <li><i class="fas fa-check fa-1x"></i>Configuramos tu tienda con el software de eCommerce Prestashop</li>
+                        <li><i class="fas fa-check fa-1x"></i>Puedes comprar plantillas de diseño disponibles en la tienda de Prestashop o creamos una para ti</li>
+                        <li><i class="fas fa-check fa-1x"></i>Puedes proporcionarnos tus propios banners o te los diseñamos</li>
+                        <li><i class="fas fa-check fa-1x"></i>Tendrás acceso al panel de administración donde podrás añadir tus productos, precios, métodos de transporte...</li>
+                        <li><i class="fas fa-check fa-1x"></i>Posicionamos tu tienda en Google</li>
+                        <li><i class="fas fa-check fa-1x"></i>Mantenimiento gratuito</li>
+                    </ul>
+            </div>
+        </div>
+    </div>
+
+</section>
