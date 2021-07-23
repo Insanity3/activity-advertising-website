871d3fad1fa25b16e1739a505e4492bf1f172af9
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-23 15:47:29 +0200

    Añadir sección renovación de web v1

---
+++ Front/Views/Sections/renovacion.php
@@ -0,0 +1,21 @@
+<section>
+<a name="renovacion"></a>
+    <div class="container">
+        <div class="containerduo">
+            <div class="flexcentercolumn duo back">
+                    <h2>Renovación</h2>
+                    <i class="fas fa-magic fa-8x imovil"></i>
+                    <h3 class="textcenter">¿Tu web se ha quedado anticuada?</h3>
+                    <h4>Te ofrecemos:</h4>
+                    <ul>
+                        <li><i class="fas fa-check fa-1x"></i>Si tu web tiene codigo obsoleto lo actualizamos</li>
+                        <li><i class="fas fa-check fa-1x"></i>Adaptamos el diseño a las últimas tendencias</li>
+                    </ul>
+            </div>
+            <div class="flexcentercolumn duo color">
+            <img src="Front/Public/Img/anticuada.jpg" class="imgcenter" alt="Web anticuada">
+            </div>
+        </div>
+    </div>
+
+</section>
