2db1d13ccf96bf8366c97a889666fb639dd97a39
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-23 16:01:42 +0200

    Añadir sección nueva web v1

---
+++ Front/Views/Sections/nuevaweb.php
@@ -0,0 +1,25 @@
+<section>
+<a name="new"></a>
+    <div class="container">
+        <div class="containerduo">
+            <div class="flexcentercolumn duo color dos">
+                <img src="Front/Public/Img/webmoderna.gif" class="imgcenter" alt="Web moderna">
+            </div>
+            <div class="flexcentercolumn duo back uno">
+                    <h2>Tu nueva web</h2>
+                    <i class="fas fa-palette fa-8x imovil"></i>
+                    <h3 class="textcenter">¡Nos encargamos de crear la web que necesites!</h3>
+                    <h4>Te ofrecemos:</h4>
+                    <ul>
+                        <li><i class="fas fa-check fa-1x"></i>Web corporativa</li>
+                        <li><i class="fas fa-check fa-1x"></i>Portfolio digital</li>
+                        <li><i class="fas fa-check fa-1x"></i>Blog</li>
+                        <li><i class="fas fa-check fa-1x"></i>Foro</li>
+                        <li><i class="fas fa-check fa-1x"></i>Si tienes una idea diferente a las previamente listadas no dudes en comunicarla</li>
+
+                    </ul>
+            </div>
+        </div>
+    </div>
+
+</section>
