11b8e24abbbae62eb9c40f273d97fdd79db6b0f1
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-29 13:16:02 +0200

    Añadir sección seo v1

---
+++ Front/Views/Sections/seo.php
@@ -0,0 +1,6 @@
+<section class="seo">
+<h2>Posicionamiento web</h2>
+<i class="fas fa-search fa-6x"></i>
+<h3 class="textcenter">Sea cual sea el tipo de web que elijas es importante que aparezcas en los primeros resultados de las búsquedas que hagan tus potenciales clientes</h3>
+<p class="textcenter">En InsanityDev-Desarrollo web profesional nos encargaremos de que tu web cumpla con todos los requisitos para que sea atractiva tanto para los motores de búsqueda como para tus potenciales clientes o visitantes y así posicionarte en los primeros lugares en los resultados de búsqueda</p>
+</section>
