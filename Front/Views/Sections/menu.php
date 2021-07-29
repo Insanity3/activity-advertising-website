573b5bff4e40353096d41e70dd3153c94a203974
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2021-07-29 15:43:55 +0200

    Añadir menú v1

---
+++ Front/Views/Sections/menu.php
@@ -0,0 +1,21 @@
+<header>
+    <nav>
+        <a href="#">
+            <img src="Front/Public/Img/logo.png" alt="logo-InsanityDev-Desarrollo-web" class="logo">
+        </a>
+        <ul class="enlaces-menu">
+            <li><a href="#inicio">Inicio</a></li>
+            <li><a href="#qr">Carta QR</a></li>
+            <li><a href="#ecommerce">eCommerce</a></li>
+            <li><a href="#renovacion">Renovación web</a></li>
+            <li><a href="#new">Nueva web</a></li>
+            <li><a href="#seo">SEO</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+        </ul>
+        <button class="ham" type="button">
+            <span class="br-1"></span>
+            <span class="br-2"></span>
+            <span class="br-3"></span>
+        </button>
+    </nav>
+</header>
